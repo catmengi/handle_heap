38e8c68d8b183f6ecd9a5a511e8bb945b2026b47
Catmengi <138163939+catmengi@users.noreply.github.com>
2025-07-20 14:31:07 +0300

    Add files via upload

---
+++ mm_handles.c
@@ -0,0 +1,401 @@
+#include <assert.h>
+#include <stdatomic.h>
+#include <pthread.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "mm_handles.h"
+
+static void qsort_impl(void* base, size_t num, size_t size, int (*compare) (const void *, const void *)){
+    qsort(base,num,size,compare);
+}
+
+static void random_buf(void* buf, size_t nbytes){
+    arc4random_buf(buf,nbytes);
+}
+
+enum{
+    MM_HANDLE_SERIALIZE_FUNC,
+    MM_HANDLE_DESERIALIZE_FUNC,
+    MM_HANDLE_META_MAX,
+}mm_metadata_blocks;
+
+typedef struct _mm_handle_metablock{
+    int huid; //handle unique ID
+    uintptr_t metadata[MM_HANDLE_META_MAX]; //userdefined metadata;
+
+    int alloc_size; //size of memory controlled by handle
+    short alligned_size; //size of allocation in blocks
+    uint8_t* ptr;     //pointer to chunk of memory
+
+    struct{
+        struct{
+            void (*incref)(mm_handle handle, void* udata);
+            void (*decref)(mm_handle handle, void* udata);
+            int (*zero)(mm_handle handle); //return NON 0 if you need to stop further free
+        }callbacks;
+
+        void* ctx;
+        atomic_int counter;
+    }refcount;
+
+    pthread_mutex_t lock;
+    bool used;
+}mm_handle_metablock;
+
+
+
+#define HANDLE_HEAP_SIZE (1 * 1024 * 1024)
+#define HANDLE_HEAP_MINALLOC 128
+
+static pthread_mutex_t g_heap_mutex = PTHREAD_MUTEX_INITIALIZER;
+static mm_handle_metablock g_handle_metablocks[HANDLE_HEAP_SIZE / HANDLE_HEAP_MINALLOC] = {0};
+static uint8_t g_handle_heap[HANDLE_HEAP_SIZE] = {0};
+
+static size_t g_blocks_availible = sizeof(g_handle_metablocks) / sizeof(g_handle_metablocks[0]);
+
+__attribute__((constructor))
+static void init_metablocks_lock(void){
+    pthread_mutexattr_t attr;
+    pthread_mutexattr_init(&attr);
+    pthread_mutexattr_settype(&attr,PTHREAD_MUTEX_RECURSIVE);
+
+    pthread_mutex_init(&g_heap_mutex,&attr);
+
+    for(int i = 0; i < sizeof(g_handle_metablocks) / sizeof(g_handle_metablocks[0]); i++){
+        pthread_mutex_init(&g_handle_metablocks[i].lock,&attr);
+    }
+
+    pthread_mutexattr_destroy(&attr);
+}
+
+static mm_handle_metablock* find_free_metablock(){
+    pthread_mutex_lock(&g_heap_mutex);
+
+    mm_handle_metablock* free_block = NULL;
+
+    for(int i = 0; i < sizeof(g_handle_metablocks) / sizeof(g_handle_metablocks[0]); i++){
+        if(g_handle_metablocks[i].used == false){
+            free_block = &g_handle_metablocks[i];
+            break;
+        }
+    }
+
+    pthread_mutex_unlock(&g_heap_mutex);
+    return free_block;
+}
+
+static uintptr_t nextby (uintptr_t value, uintptr_t by) {
+    if (value % by == 0) {
+        return value; // Already divisible by
+    }
+    return (value / by + 1) * by; // Calculate the next multiple of by
+}
+
+static void* find_free_heap_start(int size){
+    pthread_mutex_lock(&g_heap_mutex);
+
+    uint8_t* free_start = &g_handle_heap[0];
+    int alligned_size = nextby(size, HANDLE_HEAP_MINALLOC);
+
+
+    for(int i = 0; i < sizeof(g_handle_metablocks) / sizeof(g_handle_metablocks[0]); i++){
+        if(g_handle_metablocks[i].used == true){
+            if(free_start <= g_handle_metablocks[i].ptr){
+                free_start = g_handle_metablocks[i].ptr + g_handle_metablocks[i].alligned_size;
+
+                if(free_start >= &g_handle_heap[HANDLE_HEAP_SIZE] || &g_handle_heap[HANDLE_HEAP_SIZE] - free_start < alligned_size) {
+                    pthread_mutex_unlock(&g_heap_mutex);
+                    return NULL;
+                }
+            }
+        }
+    }
+
+    pthread_mutex_unlock(&g_heap_mutex);
+    return free_start;
+}
+
+static int compactor_sort(const void* a, const void* b){
+    mm_handle_metablock* metablock_a = &g_handle_metablocks[*(int*)a];
+    mm_handle_metablock* metablock_b = &g_handle_metablocks[*(int*)b];
+
+    if(metablock_a->ptr < metablock_b->ptr) return -1;
+    if(metablock_a->ptr > metablock_b->ptr) return 1;
+
+    return 0;
+}
+
+static void compact_heap(){
+    pthread_mutex_lock(&g_heap_mutex);
+
+    printf("triggered compact!\n");
+
+    int sorted_indices[sizeof(g_handle_metablocks) / sizeof(g_handle_metablocks[0])];
+    for(int i = 0; i < sizeof(sorted_indices) / sizeof(sorted_indices[0]); i++){
+        sorted_indices[i] = i; //initialise this array first
+    }
+
+    //then do qsort on it, but not based on index, instead being based on g_handle_metablocks[index].ptr
+    qsort_impl(sorted_indices,sizeof(sorted_indices) / sizeof(sorted_indices[0]), sizeof(sorted_indices[0]),compactor_sort);
+
+    //now loop on sorted_indices to compact heap
+    uint8_t* move_to = &g_handle_heap[0]; //move block to that pointer, then increase it by alligned_size
+    for(int i = 0; i < sizeof(sorted_indices) / sizeof(sorted_indices[0]); i++){
+
+        //we can be sure that number of elements in sorted_indices == number of elements in g_handle_metablocks
+        if(g_handle_metablocks[sorted_indices[i]].used && move_to != g_handle_metablocks[sorted_indices[i]].ptr){
+
+            pthread_mutex_lock(&g_handle_metablocks[sorted_indices[i]].lock);
+
+            memmove(move_to,g_handle_metablocks[sorted_indices[i]].ptr,g_handle_metablocks[sorted_indices[i]].alligned_size);
+            g_handle_metablocks[sorted_indices[i]].ptr = move_to;
+
+            move_to += g_handle_metablocks[sorted_indices[i]].alligned_size;
+
+            pthread_mutex_unlock(&g_handle_metablocks[sorted_indices[i]].lock);
+        }
+    }
+
+    pthread_mutex_unlock(&g_heap_mutex);
+}
+
+mm_handle mm_alloc(size_t size){
+    assert(size != 0);
+
+    pthread_mutex_lock(&g_heap_mutex);
+
+    assert(g_blocks_availible > 0);
+    g_blocks_availible -= (nextby(size,HANDLE_HEAP_MINALLOC) / HANDLE_HEAP_MINALLOC);
+
+    mm_handle_metablock* metablock = find_free_metablock();
+
+    int retry_count = 0;
+    while((metablock->ptr = find_free_heap_start(size)) == NULL){
+        compact_heap();
+        assert(retry_count++ == 0 && "Cannot allocate enough space, FIX API TO NOT CRASH WHOLE DEVICE!");
+    }
+
+    metablock->alloc_size = size;
+    metablock->alligned_size = nextby(metablock->alloc_size,HANDLE_HEAP_MINALLOC);
+
+    random_buf(&metablock->huid,sizeof(metablock->huid));
+
+    memset(metablock->metadata,0,sizeof(metablock->metadata));
+
+    metablock->used = true;
+
+    mm_handle handle = {
+        .huid = metablock->huid,
+        .info = metablock,
+    };
+
+    pthread_mutex_unlock(&g_heap_mutex);
+
+    return handle;
+}
+
+size_t mm_size(mm_handle handle){
+    if(handle.info && handle.huid == handle.info->huid){
+
+        auto_unlock mm_handle auto_handle = handle;
+        mm_lock(auto_handle);
+
+        return handle.info->alloc_size;
+    }
+    assert(0 && "Usage of invalid handle! mm_size");
+}
+
+mm_handle mm_realloc(mm_handle handle, size_t size){
+    pthread_mutex_lock(&g_heap_mutex);
+
+    auto_unlock mm_handle auto_handle = handle;
+    mm_lock(auto_handle);
+
+    if(auto_handle.info && auto_handle.huid == auto_handle.info->huid && size != 0){
+
+        int new_alligned_size = nextby(size,HANDLE_HEAP_MINALLOC);
+
+        mm_handle_metablock* mblock = auto_handle.info;
+
+        if(mblock->alligned_size == new_alligned_size){
+            mblock->alloc_size = size;
+
+            pthread_mutex_unlock(&g_heap_mutex);
+            return auto_handle;
+        } else {
+            if(mblock->alligned_size > new_alligned_size){
+                mblock->alligned_size = new_alligned_size;
+                mblock->alloc_size = size;
+
+                pthread_mutex_unlock(&g_heap_mutex);
+                return auto_handle;
+            }
+
+            if(mblock->alligned_size < new_alligned_size){
+                uint8_t* check_is_empty = mblock->ptr + new_alligned_size;
+                bool unused = true;
+
+                for(int i = 0; i < sizeof(g_handle_metablocks) / sizeof(g_handle_metablocks[0]); i++){
+                    if(g_handle_metablocks[i].ptr == check_is_empty){
+                        unused = false;
+                        break;
+                    }
+                }
+
+                if(unused){
+                    mblock->alligned_size = new_alligned_size;
+                    mblock->alloc_size = size;
+
+                    pthread_mutex_unlock(&g_heap_mutex);
+                    return auto_handle;
+                } else {
+                    auto_unlock mm_handle new_handle = mm_alloc(size);
+                    auto_unlock mm_handle old_handle = auto_handle; //copying this to properly unlock it in future
+
+                    memcpy(mm_lock(new_handle),mm_lock(old_handle),mm_size(old_handle));
+
+                    mm_free(old_handle);
+                    pthread_mutex_unlock(&g_heap_mutex);
+                    return new_handle;
+                }
+
+            }
+        }
+    }
+
+    pthread_mutex_unlock(&g_heap_mutex);
+    return (mm_handle){0};
+}
+
+void* mm_lock(mm_handle handle){
+    if(handle.info && handle.info->huid == handle.huid){
+        pthread_mutex_lock(&handle.info->lock);
+        return handle.info->ptr;
+    }
+
+    return NULL;
+}
+
+void mm_unlock(mm_handle handle){
+    if(handle.info && handle.info->huid == handle.huid){
+        pthread_mutex_unlock(&handle.info->lock);
+    }
+}
+
+void mm_auto_unlock(mm_handle* handle){
+    mm_unlock(*handle);
+}
+
+void mm_set_incref_cb(mm_handle handle, void (*incref)(mm_handle handle, void* udata)){
+    auto_unlock mm_handle auto_handle = handle; //auto_handle is equal to handle, but it will be automaticly unlocked at scope exit, mm_lock return NULL if handle is invalid, mm_unlock will ignore invalid handles!
+    if(mm_lock(auto_handle)){
+        auto_handle.info->refcount.callbacks.incref = incref;
+    }
+}
+void mm_set_decref_cb(mm_handle handle, void (*decref)(mm_handle handle, void* udata)){
+    auto_unlock mm_handle auto_handle = handle;
+    if(mm_lock(auto_handle)){
+        auto_handle.info->refcount.callbacks.decref = decref;
+    }
+}
+void mm_set_zero_cb(mm_handle handle, int (*zero)(mm_handle handle)){
+    auto_unlock mm_handle auto_handle = handle;
+    if(mm_lock(auto_handle)){
+        auto_handle.info->refcount.callbacks.zero = zero;
+    }
+}
+
+void mm_set_refcount_ctx(mm_handle handle, void* ctx){
+    auto_unlock mm_handle auto_handle = handle;
+    if(mm_lock(auto_handle)){
+        auto_handle.info->refcount.ctx = ctx;
+    }
+}
+
+void* mm_get_refcount_ctx(mm_handle handle){
+    auto_unlock mm_handle auto_handle = handle;
+    if(mm_lock(auto_handle)){
+        return auto_handle.info->refcount.ctx;
+    }
+    return NULL;
+}
+
+atomic_int mm_get_refcount(mm_handle handle){
+    auto_unlock mm_handle auto_handle = handle;
+    if(mm_lock(auto_handle)){
+        return auto_handle.info->refcount.counter;
+    }
+    return -1;
+}
+
+mm_handle mm_incref(mm_handle handle, void* udata){
+    auto_unlock mm_handle auto_handle = handle;
+    if(mm_lock(auto_handle)){
+        if(auto_handle.info->refcount.callbacks.incref){
+
+            auto_handle.info->refcount.counter++;
+            auto_handle.info->refcount.callbacks.incref(auto_handle,udata);
+
+            return handle;
+        }
+    }
+    return (mm_handle){0};
+}
+
+void mm_decref(mm_handle handle, void* udata){
+    auto_unlock mm_handle auto_handle = handle;
+    if(mm_lock(auto_handle)){
+        mm_handle_metablock* metablock = handle.info;
+
+        if(metablock->refcount.counter > 0){
+            metablock->refcount.counter--;
+
+            if(metablock->refcount.callbacks.decref){
+                metablock->refcount.callbacks.decref(handle,udata);
+            }
+        }
+        if(metablock->refcount.counter == 0){
+            mm_free(handle); //this will call zero callback if it exists!
+        }
+    }
+}
+
+void mm_zeroout(mm_handle handle){
+    auto_unlock mm_handle auto_handle = handle;
+    if(mm_lock(auto_handle)){
+        if(auto_handle.info->refcount.callbacks.zero){
+            if(auto_handle.info->refcount.callbacks.zero(auto_handle) == 0){
+                auto_handle.info->refcount.counter = 0;
+            }
+        }
+    }
+}
+
+void mm_free(mm_handle handle){
+    auto_unlock mm_handle auto_handle = handle;
+    if(mm_lock(auto_handle)){
+        mm_handle_metablock* mblock = auto_handle.info;
+
+        int disallow_further_free = 0; //reversed logic beucase zero callback should return NON 0 value if error
+        if(mblock->refcount.callbacks.zero){
+            disallow_further_free = mblock->refcount.callbacks.zero(auto_handle); //trying to call zero callback on free!
+        }
+
+        if(disallow_further_free == 0){
+            random_buf(&mblock->huid,sizeof(mblock->huid)); //invalidate previos HUID
+
+            size_t alligned_size = mblock->alligned_size;
+            mblock->used = false;
+
+            g_blocks_availible += alligned_size / HANDLE_HEAP_MINALLOC; //return N blocks to heap as availible
+        }
+    }
+}
+
+size_t mm_availiblemem(){
+    return g_blocks_availible * HANDLE_HEAP_MINALLOC;
+}
---
+++ mm_handles.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <sys/types.h>
+#include <stdatomic.h>
+
+typedef struct _mm_handle_metablock mm_handle_metablock;
+
+typedef struct{
+    int huid; //after mm_handle invalidation it would be changed in metablock and all handles with huid != info->huid will cause assert!
+    mm_handle_metablock* info;
+}mm_handle;
+
+//================= PRIVATE API ================================
+
+void mm_auto_unlock(mm_handle* handle); //same as unlock but exist only for auto_unlock macro
+
+//==============================================================
+
+#define auto_unlock __attribute__((cleanup(mm_auto_unlock))) //defined to automaticly unlock handle at scope exit
+
+mm_handle mm_alloc(size_t size); //allocate memory sized by size, allocation is alligned based on internal define of HANDLE_HEAP_MINALLOC
+mm_handle mm_realloc(mm_handle handle, size_t size); //realloc handle to size
+void* mm_lock(mm_handle handle); //locks handle, prevents this block from being moved or accessed from another thread;
+void mm_unlock(mm_handle handle);//unlocks handle
+size_t mm_availiblemem(); //return ammount of availible memory in bytes!
+
+void mm_set_incref_cb(mm_handle handle, void (*incref)(mm_handle handle, void* udata)); //sets handle callback on incref
+void mm_set_decref_cb(mm_handle handle, void (*decref)(mm_handle handle, void* udata)); //sets handle callback on decref
+void mm_set_zero_cb(mm_handle handle, int (*zero)(mm_handle handle)); //sets handle callback that will be executed when handle's refcount reaches 0; zero callback should return 0 on success, otherwise any NON 0 value
+void mm_set_refcount_ctx(mm_handle handle, void* ctx); //sets handle ctx that can be accessed from refcount callbacks
+
+void* mm_get_refcount_ctx(mm_handle handle); //get handle's context for callbacks set by mm_set_refcount_ctx
+atomic_int mm_get_refcount(mm_handle handle); //get handle refcount
+size_t mm_size(mm_handle handle); //returns allocation size of handle (not alligned_size)
+
+mm_handle mm_incref(mm_handle handle, void* udata); //increse refcount and call incref callback
+void mm_decref(mm_handle handle, void* udata); //decrese refcount and call decref callback, if refcount reaches 0 -> call mm_free!
+void mm_free(mm_handle handle); //call zero callback and free memory that handle use. If zero callback return error (NON 0 value) handle will not be freed
+void mm_zeroout(mm_handle handle); //calls zero callback, if zero callback returned success(0), then drops refcount to 0
